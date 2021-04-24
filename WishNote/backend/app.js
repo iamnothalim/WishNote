6cec2b4ab3b65256a14623c6421cf42561e09ae8
Jemerald97 <betty970823@naver.com>
2021-04-24 21:38:15 +0900

    21.04.24 je 소스트리 복습

--- WishNote/backend/app.js
+++ WishNote/backend/app.js
@@ -1,22 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const mongoose = require('mongoose');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
 
-var mainRouter = require('./routes/main');
-var authRouter = require('./routes/auth');
+var mainRouter = require("./routes/main");
+var authRouter = require("./routes/auth");
 
 var app = express();
 
+//21.04.24 je
 
 //mongoDB
 mongoose
-  .connect('mongodb://132.226.18.214:27017/wishnote', { 
-    useNewUrlParser: true, 
+  .connect("mongodb://132.226.18.214:27017/wishnote", {
+    useNewUrlParser: true,
     useFindAndModify: false,
-    useUnifiedTopology: true 
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("몽고디비 오랜만이얌");
@@ -25,34 +26,33 @@ mongoose
     console.error(e);
   });
 
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', mainRouter);
-app.use('/auth', authRouter);
+app.use("/", mainRouter);
+app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
