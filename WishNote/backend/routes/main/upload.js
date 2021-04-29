f0a6dcee02070e17c103f10c09b22b5dceebb7ff
iamnothalim <iamnothalim@naver.com>
2021-04-30 00:05:52 +0900

    upload

---
+++ WishNote/backend/routes/main/upload.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const multer = require('multer');
+const path = require('path');
+
+const router = express.Router();
+
+// multer setting
+const upload = multer({
+    storage: multer.diskStorage({
+        // set a localstorage destination
+        destination: (req, file, cb) => {
+            cb(null, 'uploads/');
+        },
+        // convert a file name
+        filename: (req, file, cb) => {
+            cb(null, new Date().valueOf() + path.extname(file.originalname));
+        },
+    }),
+});
+
+router.post('/', upload.single('img'),(req,res)=>{
+    console.log('/api/upload 라우팅 호출');
+    console.log(req.file.filename);
+    console.log(req.file.path);
+} );
+
+module.exports = router;
