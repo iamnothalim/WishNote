bc6cd7f7b90d7feaeb7e6ba37e5580f0b9da20c8
Jemerald97 <betty970823@naver.com>
2021-05-04 15:19:24 +0900

    21.05.04 JE  야호

--- WishNote/backend/routes/auth/check.js
+++ WishNote/backend/routes/auth/check.js
@@ -4,28 +4,27 @@ const User = require("../../models/user");
 
 router.get("/", async function (req, res, next) {
   console.log("여긴 check");
-  console.log('asdas',req.user)
+  console.log("asdas", req.user);
   if (!req.user) {
     res.json({ id: null });
     return;
-  }else {
+  } else {
     try {
-      const userdata= await User.findByUserId(req.user.id);
-      
-        const user ={
-          nickname:userdata.nickname,
-          name:userdata.name,
-          id:userdata.id,
-          point:userdata.point
-        }
-        console.log(user);
-        res.json(user);
-      
+      const userdata = await User.findByUserId(req.user.id);
+
+      const user = {
+        nickname: userdata.nickname,
+        name: userdata.name,
+        id: userdata.id,
+        point: userdata.point,
+      };
+      console.log(user);
+      res.json(user);
     } catch (e) {
       console.log(e.message);
       res.status(500).send("Server Error");
     }
   }
 });
- //const user = req.user;
+//const user = req.user;
 module.exports = router;
